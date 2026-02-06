f8cc939e9e12097ca96d80c2a514cbd5da182871
noahffiliation <nlindsey15@gmail.com>
2026-02-05 21:36:14 -0600

    Restructure project

---
+++ src/main/java/com/quizzicle/Test.java
@@ -0,0 +1,22 @@
+package com.quizzicle;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.FileInputStream;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        FileInputStream inputstream = new FileInputStream("input/csTerms.txt");
+        CharStream input = CharStreams.fromStream(inputstream);
+        QuizzicleLexer lexer = new QuizzicleLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        QuizzicleParser parser = new QuizzicleParser(tokens);
+        ParseTree tree = parser.file();
+
+        QuizzicleVizz visitor = new QuizzicleVizz();
+        visitor.visit(tree);
+    }
+}
